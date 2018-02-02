4c95f42d21ff6ddeabbedcce7deed9acc60c9268
gmarkay <gmarkay@gmail.com>
2018-02-02 11:27:47 -0600

    fixed new board ctrl

--- app/controllers/NewBoardCtrl.js
+++ app/controllers/NewBoardCtrl.js
@@ -1,21 +1,18 @@
 'use strict';
 
 angular
-    .module('pinterest')
-    .controller('NewBoardCtrl', function ($scope, BoardFactory, $window) {
-        $scope.sup = 'sup';
-        $scope.board = {
-            title: ''
-        };
-        $scope.CreateBoard = () => {
-            $scope.board.uid  = firebase.auth().currentUser.uid;
-            BoardFactory.addNewBoard($scope.board)
-                .then((data) => {
-<<<<<<< HEAD
-=======
-                    console.log('newboard', data);
->>>>>>> master
-                    $window.location.href = '#!/BoardList';
-                });
-        };
-    });
+  .module('pinterest')
+  .controller('NewBoardCtrl', function ($scope, BoardFactory, $window) {
+    $scope.sup = 'sup';
+    $scope.board = {
+      title: ''
+    };
+    $scope.CreateBoard = () => {
+      $scope.board.uid = firebase.auth().currentUser.uid;
+      BoardFactory.addNewBoard($scope.board)
+        .then((data) => {
+          console.log('newboard', data);
+          $window.location.href = '#!/BoardList';
+        });
+    };
+  });
